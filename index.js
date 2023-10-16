5dc6791cc717391f2112387a27832e4002641e8b
JohnB08 <johnb08.kodehode@gmail.com>
2023-10-16 10:00:19 +0200

    added some comments

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // Overlay effect
-
+//Fetching elements
 const btn = document.getElementById("displaybtn");
 const bg = document.getElementById("overlay");
+
+//eventlistener
 btn.addEventListener("click", (event) => {
   overlay.style.display = "grid";
 });
+//eventlistener with high spesificity for a div
 document.addEventListener("click", (event) => {
   //lurt triks for å spesifisere at det er BARE bakgrunnen, og ikke barn av bakgrunnen som dette gjelder for.
   if (event.target === bg) {
